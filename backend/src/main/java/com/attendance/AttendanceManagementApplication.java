56aceb195935e776804e2a2ed92fd13f141017a7
kalapalalakshmi29 <kalapalalakshmi183@gmail.com>
2026-06-21 14:11:47 +0530

    Update AttendanceManagementApplication.java

--- backend/src/main/java/com/attendance/AttendanceManagementApplication.java
+++ backend/src/main/java/com/attendance/AttendanceManagementApplication.java
@@ -2,7 +2,7 @@ package com.attendance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+ 
 @SpringBootApplication
 public class AttendanceManagementApplication {
     public static void main(String[] args) {
