ffc522598fb97b7d269e7208c491d58e9470161d
kalapalalakshmi29 <kalapalalakshmi183@gmail.com>
2026-06-21 14:17:28 +0530

    Fix import statement for io.jsonwebtoken

--- backend/src/main/java/com/attendance/security/JwtUtil.java
+++ backend/src/main/java/com/attendance/security/JwtUtil.java
@@ -1,6 +1,6 @@
 package com.attendance.security;
 
-import io.jsonwebtoken.*;
+import io.jsonwebtoken.*; 
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
