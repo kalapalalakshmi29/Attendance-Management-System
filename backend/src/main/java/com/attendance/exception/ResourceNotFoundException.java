bd6bd76b4128519be31725fffcbf764459b96bc7
kalapalalakshmi29 <kalapalalakshmi183@gmail.com>
2026-06-21 14:16:37 +0530

    Fix formatting in ResourceNotFoundException class

--- backend/src/main/java/com/attendance/exception/ResourceNotFoundException.java
+++ backend/src/main/java/com/attendance/exception/ResourceNotFoundException.java
@@ -1,5 +1,5 @@
 package com.attendance.exception;
-
+ 
 public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message) {
         super(message);
